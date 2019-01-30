14fdc803f2d2061d8475f4d6b677b655dfdb09ab
Ethan Vernon <ethan.vernon@colorado.edu>
2019-01-30 16:52:32 +0800

    Updates

---
+++ precache-manifest.5485de93bd49079e0e3a5b63691bd2f2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/npc_generator/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "9229f1a2b18767d005b9",
+    "url": "/npc_generator/static/js/runtime~main.9229f1a2.js"
+  },
+  {
+    "revision": "a423a22c6e85f96a42e1",
+    "url": "/npc_generator/static/js/main.a423a22c.chunk.js"
+  },
+  {
+    "revision": "11cd304093640138fb65",
+    "url": "/npc_generator/static/js/1.11cd3040.chunk.js"
+  },
+  {
+    "revision": "a423a22c6e85f96a42e1",
+    "url": "/npc_generator/static/css/main.8e1ba57c.chunk.css"
+  },
+  {
+    "revision": "943e8bc93951f040271ecb9112dfd859",
+    "url": "/npc_generator/index.html"
+  }
+];
